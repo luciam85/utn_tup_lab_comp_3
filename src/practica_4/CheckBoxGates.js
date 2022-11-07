83ab397e83a76a7e555a7379eb96496dec7022e2
luciam85 <lucia_mulet@hotmail.com>
2022-11-07 20:19:42 -0300

    Practicas terminadas

---
+++ src/practica_4/CheckBoxGates.js
@@ -0,0 +1,11 @@
+const CheckBoxGates = ({ checkBox }) => {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" onChange={checkBox} /> Entrada 2
+      </label>
+    </div>
+  );
+};
+
+export default CheckBoxGates;
---
+++ src/practica_4/InputGates.js
@@ -0,0 +1,9 @@
+const InputGates = ({ gateInput }) => {
+  return (
+    <div>
+      <label for="input1">Entrada 1:</label>
+      <input type="number" id="input1" onChange={gateInput} />
+    </div>
+  );
+};
+export default InputGates;
